194f283cf639afd0d04d1068de25da76c426f437
Almaz <almazzz-2@yandex.ru>
2024-03-11 21:34:13 +0300

    cornJob test

---
+++ src/api/cornJob.ts
@@ -0,0 +1,21 @@
+// export default function handler(req, res) {
+//   res.status(200).end('Hello Cron!');
+// }
+
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  // Замените URL на URL вашего сервера
+  try {
+    const response = await fetch('https://layer-backend.onrender.com');
+    const data = await response.json();
+
+    // Обработайте полученные данные как необходимо
+    console.log(data);
+
+    // Верните ответ, если это необходимо
+    return NextResponse.json({ message: 'GET request sent successfully' });
+  } catch (error) {
+    console.log(error);
+  }
+}
